93519face06005472f80ea0a67d07470577387f5
Olena Borysenko <elena.lysyh@gmail.com>
2020-05-22 16:49:17 +0300

    delete test

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header  className='header'>
+            <img src='https://alllogos.ru/images/logotip-ghostbuster.jpg'/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <nav className='nav'>
+            <div>
+                <a>Profile</a>
+            </div>
+            <div>Messages</div>
+            <div>
+                <a>News</a>
+            </div>
+            <div>
+                <a>Music</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
